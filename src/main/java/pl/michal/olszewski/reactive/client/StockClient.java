7bc3fb0e7b6d0f2c07d5edb5d8065d953bf97509
Michał <olszewskimichal@outlook.com>
2018-05-07 21:11:38 +0200

    drobne zmiany w kliencie giełdowym

--- src/main/java/pl/michal/olszewski/reactive/client/StockClient.java
+++ src/main/java/pl/michal/olszewski/reactive/client/StockClient.java
@@ -2,15 +2,22 @@ package pl.michal.olszewski.reactive.client;
 
 import io.reactivex.Observable;
 import io.reactivex.schedulers.Schedulers;
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class StockClient {
 
-  static public void main(final String[] args) throws InterruptedException {
+  static Map<String, BigDecimal> prices = new ConcurrentHashMap<>();
+
+  static public void main(final String[] args) {
     final Retrofit retrofit = new Retrofit.Builder()
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .addConverterFactory(GsonConverterFactory.create())
@@ -20,7 +27,7 @@ public class StockClient {
     final ExecutorService executorService = Executors.newFixedThreadPool(4);
     final StockRxApi stockService = retrofit.create(StockRxApi.class);
 
-    stockService.getMostActiveStocks()
+   /* stockService.getMostActiveStocks()
         .subscribeOn(Schedulers.from(executorService))
         .flatMapIterable(v -> v)
         .flatMap(v -> {
@@ -29,15 +36,44 @@ public class StockClient {
         })
         .subscribe(System.out::println);
 
-    Observable.just("AAPL", "MSFT", "GOOGL", "NFLX")
+    Observable.just("AAPL", "MSFT", "GOOGL", "NFLX").repeat()
         .subscribeOn(Schedulers.from(executorService))
         .flatMap(stockService::getCompanyInfoForName)
-        .subscribe(v -> System.out.println(Thread.currentThread() + " " + v));
+        .subscribe(v -> System.out.println(Thread.currentThread() + " " + v));*/
 
-    Observable.just("AAPL", "MSFT", "GOOGL", "NFLX")
+   /* Observable.interval(1, TimeUnit.SECONDS).startWith(0L)
+        .zipWith(Observable.just("AAPL", "MSFT", "GOOGL", "NFLX").repeat(), (e1, e2) -> e2)
         .subscribeOn(Schedulers.from(executorService))
         .flatMap(stockService::getStockForCompany)
-        .subscribe(v -> System.out.println(Thread.currentThread() + " " + v));
-    
+        .subscribe(System.out::println);*/
+
+    Observable.just("AAPL", "MSFT", "GOOGL", "NFLX")
+        .flatMap(stockService::getStockForCompany)
+        .subscribeOn(Schedulers.from(executorService))
+        .subscribe(v -> System.out.println(v.symbol + " " + v.getLatestPrice()));
+
+    Observable.just("AAPL", "MSFT", "GOOGL", "NFLX")
+        .flatMap(stockService::getPriceForCompany)
+        .subscribeOn(Schedulers.from(executorService))
+        .subscribe(System.out::println);
+
+    Observable.just("AAPL", "MSFT", "GOOGL", "NFLX")
+        .flatMap(stockService::getStockForCompany)
+        .subscribeOn(Schedulers.from(executorService))
+        .blockingSubscribe(v -> prices.put(v.symbol, v.getDelayedPrice()));
+
+    Observable.interval(1, TimeUnit.SECONDS).startWith(0L)
+        .zipWith(Observable.just("AAPL", "MSFT", "GOOGL", "NFLX").repeat(), (e1, e2) -> e2)
+        .subscribeOn(Schedulers.from(executorService))
+        .flatMap(stockService::getStockForCompany)
+        .map(v -> {
+          BigDecimal before = prices.get(v.symbol);
+          if (v.getLatestPrice().compareTo(before) != 0) {
+            prices.put(v.symbol, v.getLatestPrice());
+            return Optional.ofNullable(v);
+          }
+          return Optional.empty();
+        }).subscribe(key -> key.ifPresent(val -> System.out.println("Nowa wartosc " + val)));
+
   }
 }
